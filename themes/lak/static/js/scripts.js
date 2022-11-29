591d87947e865672b018792d7ca015612dc172a7
lakotelman <la.n.kot@pm.me>
2022-11-28 17:27:39 -0900

    messing with mobile nav

--- themes/lak/static/js/scripts.js
+++ themes/lak/static/js/scripts.js
@@ -1,40 +1,25 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+// Burger menu 
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
+
+let toggle = document.getElementById("nav-button")
+let menu = document.getElementById("navbar-default")
+let active = false
+
+toggle.addEventListener("click", function(e)
+{
+    console.log("clickety")
+    menu.classList.remove("hidden")
+    toggle.classList.add("hidden")
+    active = true
+}
+)
+
+document.addEventListener("click", function(e){ 
+    if(e.target != menu && e.target != toggle && toggle.classList.contains("hidden")){ 
+        menu.classList.add("hidden")
+        console.log(e.target)
+        toggle.classList.remove("hidden")
+        active = false;
+        console.log("clicked outside")
     }
-
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-});
+})
